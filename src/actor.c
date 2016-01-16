2de4245ce3ef3728746d86e097ed7d219f68d045
David Bergeron <david.f.bergeron@gmail.com>
2016-01-15 18:07:48 -0700

    started a really simple actor Factory, not finished

---
+++ src/actor.c
@@ -0,0 +1,33 @@
+/*
+ * Actor.c
+ * Actor Factory
+ * WIP
+ */
+
+#include "wrecker.h"
+#include "diana.h"
+
+typedef struct comListing
+{
+    unsigned int component;
+    void *defaultValues;
+} comListing;
+
+typedef unsigned int actorID;
+
+actorID spawnActor( enum actorType )
+{
+    actorID newActor;
+    WRECK(spawn, &newActor);
+    
+    List *cList = getListComponents( actorType );
+    for (int i = 0; i < cList->count; i++){
+        comListing *new_c = (comListing*)cList[i];
+        WRECK(setComponent, newActor, new_c->component, new_c->defaultValue);
+    }
+    freeList(cList);
+}
+
+List *getListComponents( enum actorType )
+{
+}
---
+++ src/components.c
@@ -0,0 +1,9 @@
+/*
+ * File for storing the global identifiers for each component.
+ */
+
+#include "components.h"
+
+unsigned int Position;
+unsigned int Velocity;
+unsigned int Render;
