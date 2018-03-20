eb54a8d880d14d8edfd018db49366099373b762d
BrianCechmanek <bcechmanek@gmail.com>
2018-03-20 19:05:15 -0400

    local machine cleanup, missing files for posterity

---
+++ src/classes.c
@@ -0,0 +1,33 @@
+/* WRECKER
+ * classes.c 
+ * holds all classes (wizards, barbarian, rogue...)
+ * features may require tweaking
+ */
+
+// REMEMBER TO UPDATE COMPONENTS. C/H
+// AS WELL AS ECS.C
+
+#include <stdio.h>
+#include "classes.h"
+#include "wrecker.h"
+
+// make ECS unique identifies
+componentID ClassID;
+
+// Available classes!
+/* Scavenger 
+ * Demolisher 
+ * Pilot
+ * Geologist
+ * Mechanic
+ */
+
+//enums
+//structs
+
+// functions 
+void setClass(entID entityId, classes *class, int level)
+{
+    
+}
+
---
+++ src/classes.h
@@ -0,0 +1,23 @@
+// classes.h
+#pragma once
+
+#include "wrecker.h"
+
+//Enums
+enum classes {
+    SCAVENGER,
+    DEMOLISHER,
+    PILOT,
+    GEOLOGIST,
+    MECHANIC
+}
+
+//Structs
+typedef struct class_c
+{
+  
+} class_c;
+//Functions
+
+//Globals
+extern componentID classID;
